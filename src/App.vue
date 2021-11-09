898ccf0309bc2ab345fe567b187f1bd047462af0
jmdc-sfujitani <sfujitani@jmdc.co.jp>
2021-11-09 11:23:39 +0900

    ラジオボタンのコンポーネント追加

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+      <TileRadioButton
+        v-model="radio"
+        name="personal-list-radio"
+        :options="options"
+        title="個人リストやり取り方法"
+      ></TileRadioButton>
+    </div>
+</template>
+
+<script>
+import TileRadioButton from './components/TileRadioButton.vue'
+
+export default {
+  components: {
+    TileRadioButton
+  },
+  data () {
+    return {
+      radio: null,
+      options: [
+        { label: 'オンライン', value: 0 },
+        { label: 'JMDCオンライン', value: 1 },
+        { label: 'メール', value: 2 },
+        { label: '双方媒体', value: 3 },
+        { label: '健保からのみ媒体', value: 4 }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+p{
+  color: green
+}
+</style>
---
+++ src/components/TileRadioButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <fieldset>
+    <h3>{{ title }}</h3>
+    <template v-for="(option, index) in options">
+      <label :key="index">
+        <input
+          type="radio"
+          :name="name"
+          :value="option.value"
+          @change="updateValue"
+        />{{ option.label }}
+      </label>
+    </template>
+  </fieldset>
+</template>
+
+<script>
+export default {
+  props: {
+    value: { type: String, required: true },
+    options: { type: Array, required: true },
+    name: { type: String, required: true },
+    title: { type: String }
+  },
+  methods: {
+    updateValue: function (e) {
+      this.$emit('input', e.target.value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+label {
+  display: block;
+}
+</style>
